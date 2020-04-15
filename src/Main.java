527a39b124b675fa49128897bb1915fa3e2501bd
grkid <895761580@qq.com>
2020-04-15 13:55:12 +0800

    大概能跑的版本

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
-public class Main  {
-    public static void main(String[] argv)
-    {
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+
+
+public class Main  {
+    public static void main(String[] argv) throws Exception
+    {
+        Configuration conf=new Configuration();
+        Job job=new Job(conf,"Inverted Index");
+
+        job.setJarByClass(Main.class);
+
+        job.setMapperClass(Map.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setCombinerClass(Combiner.class);
+
+        job.setReducerClass(Reduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        String[] otherArgs=new GenericOptionsParser(conf,argv).getRemainingArgs();
+        if(otherArgs.length!=2)
+        {
+            System.err.println("Usage: InvertedIndex <in> <out>");
+            System.exit(2);
+        }
+
+        FileInputFormat.addInputPath(job,new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job,new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true)?0:1);
     }
 }
